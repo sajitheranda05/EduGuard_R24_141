c88b6f6a8c2fca873e89110052a755de1fd46185
Kalya <79504885+It20110734@users.noreply.github.com>
2024-10-17 04:45:01 -0300

    k

--- lib/src/features/sos_system/screens/sos_home.dart
+++ lib/src/features/sos_system/screens/sos_home.dart
@@ -1,6 +1,5 @@
 import 'package:eduguard/src/common_widgets/Screens/appbar.dart';
 import 'package:eduguard/src/common_widgets/Screens/tabbar.dart';
-import 'package:eduguard/src/features/sos_system/screens/sos_state.dart';
 import 'package:eduguard/src/features/sos_system/screens/sos_tab_section.dart';
 import 'package:flutter/material.dart';
 
@@ -15,49 +14,58 @@ class _SOSHomePageState extends State<SOSHomePage> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 3,
-        child: Scaffold(
-
-          appBar: const CustomAppBar(
-            title: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Hello, Matteo Perera...', style: TextStyle(fontSize: 14.0, color:Colors.grey),),
-                Text('Emergency SOS System.', style: TextStyle(fontSize: 16.0),)
-              ],
-            ),
-          ),
-
-          body: Column(
+      length: 3,
+      child: Scaffold(
+        appBar: const CustomAppBar(
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(
-                padding: const EdgeInsets.all(16.0),
-                child: const CustomTabBar(
-                  tabs: [
-                    Tab(child: Text('SOS'),),
-                    Tab(child: Text('Chat'),),
-                    Tab(child: Text('Location'),),
-                  ],
-                ),
+              Text(
+                'Hello, Matteo Perera...',
+                style: TextStyle(fontSize: 14.0, color: Colors.grey),
               ),
-              const Expanded(
-                  child: TabBarView(
-                    children: [
-                      Center(
-                        child: SOSTabScreen(),
-                      ),
-                      Center(
-                        child: Text('Content for Chat system'),
-                      ),
-                      Center(
-                        child: Text('Content for Location system'),
-                      ),
-                    ],
-                  )
-              ),
+              Text(
+                'Emergency SOS System.',
+                style: TextStyle(fontSize: 16.0),
+              )
             ],
           ),
         ),
+        body: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              child: const CustomTabBar(
+                tabs: [
+                  Tab(
+                    child: Text('SOS'),
+                  ),
+                  Tab(
+                    child: Text('Chat'),
+                  ),
+                  Tab(
+                    child: Text('Location'),
+                  ),
+                ],
+              ),
+            ),
+            const Expanded(
+                child: TabBarView(
+              children: [
+                Center(
+                  child: SOSTabScreen(),
+                ),
+                Center(
+                  child: Text('Content for Chat system'),
+                ),
+                Center(
+                  child: Text('Content for Location system'),
+                ),
+              ],
+            )),
+          ],
+        ),
+      ),
     );
   }
 }
